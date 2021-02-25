5ad2b6496417a62c914f597c66f56ee67dd3064f
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-26 00:27:14 +0600

    add header part

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,12 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header">
-            <h1>This is Header</h1>
             <img src={logo} alt="logo"/>
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/review">Order Review</a>
+                <a href="/manage">Manage Inventory</a>
+            </nav>
         </div>
     );
 };
