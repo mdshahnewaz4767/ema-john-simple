e0ae04087b24bd326bddd4ed83e88988181a19eb
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-04 15:57:19 +0600

    Retrieve data from localstorage

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import fakeData from '../../fakeData';
+import { getDatabaseCart } from '../../utilities/databaseManager';
 
 const Review = () => {
+
+    const [cart, setCart] = useState([])
+    //cart data
+    useEffect(() => {
+        const saveCart =  getDatabaseCart();
+        const productKeys = Object.keys(saveCart);
+
+        const cartProducts= productKeys.map(key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = saveCart[key];
+            return product;
+        })
+        // console.log(cartProducts);
+        setCart(cartProducts);
+    }, [])
     return (
         <div>
-            <h1>This is review</h1>
+            <h1>Cart Items: {cart.length}</h1>
         </div>
     );
 };
