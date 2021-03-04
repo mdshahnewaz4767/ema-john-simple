275e63a9740ca08600cdd12a03ead10f7f105bcf
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-05 01:19:41 +0600

    Sync state in multiple routes, calculate total price in review page.

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
-import  { addToDatabaseCart } from '../../utilities/databaseManager';
+import  { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
 import './Shop.css'
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     // eslint-disable-next-line no-unused-vars
-    const [products, setProducts] = useState(first10)
-    // console.log(first10);
+    const [products, setProducts] = useState(first10);
 
-    // order Summery
     const [cart, setCart] = useState([]);
 
+    useEffect(() => {
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+        const previousCart = productKeys.map(existingKey => {
+            const product = fakeData.find(pd => pd.key === existingKey);
+            product.quantity = savedCart[existingKey];
+            return product;
+        });
+        console.log(previousCart);
+        setCart(previousCart)
+    }, [])
+
+    // order Summery
     const handleAddProduct = (pd) => {
         // console.log("added", pd);
         const toBeAddedKey = pd.key;
